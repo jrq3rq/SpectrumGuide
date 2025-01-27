e177cce7b821c5b87b572428f37e646e1fece5aa
James Santos <jamesrrs.me@gmail.com>
2025-01-26 18:30:23 -0800

    Ensure .firebase is ignored

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 Spectrum Guide. All rights reserved.</p>
+      <p>© 2025 Spectrum Guide. Founded by Tatiana Reyes.</p>
     </footer>
   );
 };
