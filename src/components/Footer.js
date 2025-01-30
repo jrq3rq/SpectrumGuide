84b1575b6332746e0fb1fb66fb87937c9ca1d111
James Santos <jamesrrs.me@gmail.com>
2025-01-30 13:15:24 -0800

    icons added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/Footer.css";
+// import logo from "../assets/favicon-32x32.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
+        {/* <img src={logo} alt="Spectrum Guide Logo" className="footer-logo" /> */}
         <p>
           © 2025 <span className="brand-name">Spectrum Guide AI</span>
         </p>
